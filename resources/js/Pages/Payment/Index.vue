5d68840034cf0c1f702f3f4bc10f13ae33afa47d
Sergio <testinfonetua@gmail.com>
2022-08-11 12:13:15 +0300

    Add use creator mode

--- resources/js/Pages/Payment/Index.vue
+++ resources/js/Pages/Payment/Index.vue
@@ -1,34 +1,29 @@
 <template>
     <Head title="Dashboard" />
 
-    <AppLayout>
+    <PaymentLayout>
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 My products
             </h2>
         </template>
-
+        <div class="grid grid-cols-2 gap-4">
+            <div class="flex flex-1 justify-star px-4">
+                <h1 class="text-2xl font-semibold text-gray-900">My products</h1>
+            </div>
+            <div class="flex flex-1 justify-end">
+                <Link
+                    class="px-6 py-2 mb-2 border-x-2 border-y-2 border-black rounded"
+                    :href="route('product.create')">
+                        Product Create
+                </Link>
+          </div>
+        </div>
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <div class="mb-4">
-                            <Link
-                                class="
-                                    px-6
-                                    py-2
-                                    mb-2
-                                    text-green-100
-                                    bg-green-500
-                                    rounded
-                                "
-                                :href="route('product.create')"
-                            >
-                                Product Create
-                            </Link>
-                        </div>
+                    <div class="p-2 bg-white border-b border-gray-200">
                         <table>
-                            <thead class="font-bold bg-gray-300 border-b-2">
+                            <thead class="font-bold">
                                 <td class="px-4 py-2">Active</td>
                             </thead>
                             <tbody>
@@ -42,20 +37,19 @@
                         </table>
                         <pagination :links="payments.links" />
                     </div>
-                </div>
             </div>
         </div>
-    </AppLayout>
+    </PaymentLayout>
 </template>
 
 <script>
-import AppLayout from "@/Layouts/AppLayout.vue";
+import PaymentLayout from "@/Layouts/PaymentLayout.vue";
 import { Head } from "@inertiajs/inertia-vue3";
 import { Link } from "@inertiajs/inertia-vue3";
 import Pagination from '@/Layouts/Partials/Pagination.vue'
 export default {
     components: {
-		AppLayout,
+		PaymentLayout,
         Pagination,
 		Head,
         Link
