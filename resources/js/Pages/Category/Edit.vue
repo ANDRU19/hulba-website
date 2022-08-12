2fac7e29a3e8f605645c6f2e84b3cc3a1e91b649
Sergio <testinfonetua@gmail.com>
2022-08-12 19:08:05 +0300

    design category form

--- resources/js/Pages/Category/Edit.vue
+++ resources/js/Pages/Category/Edit.vue
@@ -1,82 +1,74 @@
 <template>
-    <Head title="Customers" />
+  <Head title="Customers" />
 
-    <AppLayout>
-        <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                Edit
-            </h2>
-        </template>
+  <AppLayout>
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight text-gray-800">Edit</h2>
+    </template>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <form @submit.prevent="submit">
-                            <div>
-                                <label for="name">Name</label>
-                                <input
-                                    type="text"
-                                    v-model="form.name"
-                                    class="
-                                        w-full
-                                        px-4
-                                        py-2
-                                        mt-2
-                                        border
-                                        rounded-md
-                                        focus:outline-none
-                                        focus:ring-1
-                                        focus:ring-blue-600
-                                    "
-                                />
-                            </div>
-
-                            <!-- submit -->
-                            <div class="flex items-center mt-4">
-                                <button
-                                    class="
-                                        px-6
-                                        py-2
-                                        text-white
-                                        bg-gray-900
-                                        rounded
-                                    "
-                                >
-                                    Save
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+    <div class="">
+      <div class="mx-auto max-w-5xl w-2/3 sm:px-6 lg:px-8">
+        <div class="bg-white">
+          <form @submit.prevent="submit">
+            <div>
+              <Label for="name">Name</Label>
+              <Input
+                type="text"
+                v-model="form.name"
+                class="
+                  w-full
+                  px-4
+                  py-2
+                  mt-2
+                  border
+                  rounded-md
+                  sm:text-sm
+                  focus:outline-none focus:ring-1 focus:ring-blue-600
+                "
+              />
             </div>
+
+            <!-- submit -->
+            <div class="flex items-center mt-4">
+              <button class="w-full px-6 py-2 text-white bg-gray-400 rounded">
+                Save
+              </button>
+            </div>
+          </form>
         </div>
-    </AppLayout>
+      </div>
+    </div>
+  </AppLayout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { Head } from "@inertiajs/inertia-vue3";
 import { useForm } from "@inertiajs/inertia-vue3";
-export default {
-    components: {
-        AppLayout,
-        Head,
-    },
-    setup(props) {
-        const form = useForm({
-            name: props.category.name,
-        });
+import Input from "@/Components/Input.vue";
+import Label from "@/Components/Label.vue";
 
-        return { form };
+export default {
+  components: {
+    AppLayout,
+    Head,
+    Label,
+    Input,
+  },
+  setup(props) {
+    const form = useForm({
+      name: props.category.name,
+    });
+
+    return { form };
+  },
+  props: {
+    category: Object,
+  },
+  methods: {
+    submit() {
+      this.form.put(route("category.update", this.category.id));
     },
-    props: {
-        category: Object,
-    },
-    methods: {
-        submit() {
-            this.form.put(route("category.update", this.category.id));
-        },
-    },
+  },
 };
 </script>
