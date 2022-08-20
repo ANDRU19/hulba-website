d6237a36209fef12f02316068ce3413c8cf3c5d5
Andrew Rybachuk <andriy.ry@gmail.com>
2022-08-20 16:24:53 +0300

    media field updatedx

--- resources/js/Pages/Products/Partials/MediaField.vue
+++ resources/js/Pages/Products/Partials/MediaField.vue
@@ -1,5 +1,47 @@
-<script setup></script>
+<script setup>
+	import { ref } from "vue";
+	import MediaFieldModal from "@/Pages/Products/Partials/MediaFieldModal.vue";
+	import Input from "@/Components/Input.vue";
+	import Label from "@/Components/Label.vue";
+	const props = defineProps({
+		modelValue: Object,
+	});
+
+	const data = ref({
+		type: "",
+		url: "",
+	});
+
+	const modalOpened = ref(false);
+</script>
 
 <template>
-	<div class="h-40 w-full border cursor-pointer border-gray-200 text-gray-400 font-light flex items-center place-content-center">Click to upload media</div>
+	<div @click="modalOpened = true" class="h-40 w-full border cursor-pointer border-gray-200 text-gray-400 font-light flex items-center place-content-center">Click to upload media</div>
+
+	<MediaFieldModal :show="modalOpened">
+		<template #content>
+			<div class="mt-5 max-w-xs">
+				<Label for="category" class="font-light text-sm">What type of media you want to upload?</Label>
+				<select v-model="data.type" class="flex-1 min-w-0 block w-full px-3 py-2 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 mt-1 placeholder-[#dedede]">
+					<option value="image">Image</option>
+					<option value="video">Video</option>
+					<option value="youtube">Youtube Link</option>
+				</select>
+			</div>
+
+			<template v-if="data.type == 'image'"> </template>
+			<template v-if="data.type == 'video'"> </template>
+			<template v-if="data.type == 'youtube'"> 
+			
+			<span class="text-gray-500 font-light text-sm mt-5">Please, insert the youtube link </span>
+			<Input placeholder="https://www.youtube.com/watch?v=lVijYeuVTY8" type="text"  class="flex-1 min-w-0 block w-full px-3 py-2 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 mt-1 placeholder-[#dedede]" v-model="data.url" autofocus />
+			
+			</template>
+
+
+		</template>
+		<template #actions>
+			<button type="button" class="place-content-centerflex place-content-center text-center items-center w-48 py-2 mb-2 px-2 text-sm text-black hover:text-white bg-[#d8ff5f] hover:bg-[#3B4D00] hover:border-[#3B4D00] rounded border border-[#abd035]" @click="modalOpened = false">Upload</button>
+		</template>
+	</MediaFieldModal>
 </template>
---
+++ resources/js/Pages/Products/Partials/MediaFieldModal.vue
@@ -0,0 +1,45 @@
+<script setup>
+	import { ref } from "vue";
+	import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from "@headlessui/vue";
+	import { XIcon } from "@heroicons/vue/outline";
+
+	const props = defineProps({
+		show: Boolean,
+	});
+
+	const open = ref(props.show);
+</script>
+
+<template>
+	<TransitionRoot as="template" :show="open">
+		<Dialog as="div" class="relative z-10" @close="open = false">
+			<TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+				<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+			</TransitionChild>
+
+			<div class="fixed z-10 inset-0 overflow-y-auto">
+				<div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+					<TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+						<DialogPanel class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
+							<div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+								<button type="button" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" @click="open = false">
+									<span class="sr-only">Close</span>
+									<XIcon class="h-6 w-6" aria-hidden="true" />
+								</button>
+							</div>
+							<div>
+								<div class="mt-3 text-center sm:mt-5 flex flex-col place-content-center items-center">
+									<DialogTitle as="h3" class="text-lg leading-6 font-medium text-gray-900"> Placing too much media makes product cool</DialogTitle>
+									<slot name="content"></slot>
+								</div>
+							</div>
+							<div class="mt-5 sm:mt-6 flex place-content-center">
+								<slot name="actions"></slot>
+							</div>
+						</DialogPanel>
+					</TransitionChild>
+				</div>
+			</div>
+		</Dialog>
+	</TransitionRoot>
+</template>
