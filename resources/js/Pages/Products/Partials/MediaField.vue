f314b103b8b7256a98eee2dfd69a0a0cf13c1b50
Andrew Rybachuk <andriy.ry@gmail.com>
2022-08-20 15:28:41 +0300

    media field updated

--- resources/js/Pages/Products/Partials/MediaField.vue
+++ resources/js/Pages/Products/Partials/MediaField.vue
@@ -1,7 +1,5 @@
 <script setup></script>
 
 <template>
-	<div class="h-40 w-40 border border-gray-200 flex items-center place-content-center">
-		<span class="text-gray-200 text-5xl material-symbols-outlined"> add_box </span>
-	</div>
+	<div class="h-40 w-full border cursor-pointer border-gray-200 text-gray-400 font-light flex items-center place-content-center">Click to upload media</div>
 </template>
